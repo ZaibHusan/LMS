a508fb86519c02f685cb39946983d00325e588f0
Hussan Zaib <222094199+ZaibHusan@users.noreply.github.com>
2025-10-21 20:25:16 -0700

    Fix filename case for Vercel build

--- src/Mainpages/User/Pages/CourseList/CourseList.jsx
+++ src/Mainpages/User/Pages/CourseList/CourseList.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../../Components/Navbar/Navbar'
 import Courselayout from './Coursecomponent/Courselayout'
+
 export default function CourseList() {
   return (
     <>
